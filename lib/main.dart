4a0867fbcf291af2755a5f0b452a36ba1d21164e
amal chandran <precisely.amal@gmail.com>
2024-09-30 12:02:45 +0530

    different ease in curve perview

--- lib/main.dart
+++ lib/main.dart
@@ -31,14 +31,39 @@ class MyApp extends StatelessWidget {
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      home: const InterpolationShowcase(),
+    );
+  }
+}
+
+class InterpolationShowcase extends StatelessWidget {
+  const InterpolationShowcase({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: const Text('Interpolation Nightmare')),
+      body: GridView.count(
+        crossAxisCount: 2,
+        mainAxisSpacing: 10,
+        crossAxisSpacing: 10,
+        children: [
+          MyHomePage(curve: Curves.linear, title: 'Linear'),
+          MyHomePage(curve: Curves.easeInOut, title: 'EaseInOut'),
+          MyHomePage(curve: Curves.bounceOut, title: 'BounceOut'),
+          MyHomePage(curve: Curves.easeInOutQuint, title: 'EaseInOutQuint'),
+          MyHomePage(curve: Curves.easeInOutBack, title: 'EaseInOutBack'),
+          MyHomePage(curve: Curves.elasticIn, title: 'ElasticIn'),
+        ],
+      ),
     );
   }
 }
 
 class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
   final String title;
+  final Curve curve;
+  const MyHomePage({super.key, required this.title, required this.curve});
 
   @override
   State<MyHomePage> createState() => _MyHomePageState();
@@ -46,6 +71,9 @@ class MyHomePage extends StatefulWidget {
 
 class _MyHomePageState extends State<MyHomePage> with TickerProviderStateMixin {
   late AnimationController _animationController;
+  late Animation<double> _animation;
+  final Duration animationDuration = Duration(seconds: 1);
+  final Duration delayDuration = Duration(seconds: 2);
 
   @override
   void initState() {
@@ -53,51 +81,85 @@ class _MyHomePageState extends State<MyHomePage> with TickerProviderStateMixin {
 
     _animationController = AnimationController(
       vsync: this,
-      duration: const Duration(milliseconds: 1000),
+      duration: animationDuration,
     );
 
-    _animationController.repeat(reverse: true);
+    _animation = Tween<double>(begin: 0.0, end: 1.0).animate(
+      CurvedAnimation(parent: _animationController, curve: widget.curve),
+    );
+
+    _startAnimation();
+    // _animationController.repeat(reverse: true);
+  }
+
+  void _startAnimation() async {
+    while (true) {
+      await _animationController.forward().orCancel;
+      await Future.delayed(delayDuration);
+      await _animationController.reverse().orCancel;
+      _animationController.reset(); // Reset to start for next iteration
+    }
+  }
+
+  @override
+  void dispose() {
+    _animationController.dispose();
+    super.dispose();
   }
 
   @override
   Widget build(BuildContext context) {
-    return AnimatedBuilder(
-        animation: Listenable.merge([
-          _animationController,
-        ]),
-        builder: (context, child) {
-          return Stack(
-            alignment: Alignment.bottomCenter,
-            children: [
-              Container(
-                width: 200,
-                height: 150,
-                margin: const EdgeInsets.only(bottom: 40),
-                decoration: BoxDecoration(
-                  color: Colors.blue,
-                  border: Border.all(color: Colors.black, width: 2),
-                ),
-              ),
-              Positioned(
-                bottom: 40, // Aligns the bottom with the parent Stack
-                child: Transform(
-                  transform: Matrix4.identity()
-                    ..setEntry(3, 2, 0.002)
-                    ..rotateX(1.3 * _animationController.value),
-                  // ..rotateX(_isExpanded ? angle * _animation.value : 0),
-                  alignment: FractionalOffset.bottomCenter,
-                  child: Container(
-                    width: 200,
-                    height: 130,
-                    decoration: BoxDecoration(
-                      color: Colors.red,
-                      border: Border.all(color: Colors.black, width: 2),
+    return Card(
+      child: Column(
+        children: [
+          Padding(
+            padding: const EdgeInsets.all(16.0),
+            child: Text(widget.title,
+                style: Theme.of(context).textTheme.titleMedium),
+          ),
+          Expanded(
+            child: AnimatedBuilder(
+              animation: Listenable.merge([
+                _animation,
+              ]),
+              builder: (context, child) {
+                return Stack(
+                  alignment: Alignment.bottomCenter,
+                  children: [
+                    Container(
+                      width: 100,
+                      height: 75,
+                      margin: const EdgeInsets.only(bottom: 40),
+                      decoration: BoxDecoration(
+                        color: Colors.blue,
+                        border: Border.all(color: Colors.black, width: 2),
+                      ),
                     ),
-                  ),
-                ),
-              ),
-            ],
-          );
-        });
+                    Positioned(
+                      bottom: 40, // Aligns the bottom with the parent Stack
+                      child: Transform(
+                        transform: Matrix4.identity()
+                          ..setEntry(3, 2, 0.002)
+                          ..rotateX(1.3 * _animation.value),
+                        // ..rotateX(_isExpanded ? angle * _animation.value : 0),
+                        alignment: FractionalOffset.bottomCenter,
+                        child: Container(
+                          width: 100,
+                          height: 60,
+                          decoration: BoxDecoration(
+                            color: Colors.red,
+                            border: Border.all(color: Colors.black, width: 2),
+                          ),
+                        ),
+                      ),
+                    ),
+                  ],
+                );
+              },
+            ),
+          ),
+        ],
+      ),
+    );
   }
 }
