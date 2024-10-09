451626f1dc48b9d630063e8bdffa5db97e1ee2e2
amal chandran <precisely.amal@gmail.com>
2024-10-09 17:30:03 +0530

    Combined animation made working now

--- lib/vinyl/vinyl.dart
+++ lib/vinyl/vinyl.dart
@@ -1,10 +1,14 @@
 import 'dart:math';
+import 'dart:ui';
 
 import 'package:flutter/material.dart';
 import 'package:flutter/physics.dart';
 import 'package:flutter/services.dart';
+import 'package:flutter/widgets.dart';
 import 'package:fx_2_folder/vinyl/camera_simulation.dart';
 import 'package:fx_2_folder/vinyl/card_stack_4_angles.dart';
+import 'package:fx_2_folder/vinyl/easing/super_duper_ease_in.dart';
+import 'package:fx_2_folder/vinyl/example_stack_animation.dart';
 import 'package:fx_2_folder/vinyl/transform_examples.dart';
 import 'package:fx_2_folder/vinyl/widget_viewer.dart';
 
@@ -13,85 +17,17 @@ class VinylHomeWidget extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(body: WidgetViewer()
-        // Stacked3DCardsLayout() //Stacked3DCardsLayout()
-        // body: CameraSimulation(),
+    return Scaffold(
+      // body: WidgetViewer()
+      // Stacked3DCardsLayout() //Stacked3DCardsLayout()
+      // body: CameraSimulation(),
 
-        // body: TransformDemo(),
+      // body: TransformDemo(),
 
-        //body: TransformApp(),
-        );
+      body: TransformApp(),
+    );
   }
 }
-
-// class MyHomePage extends StatefulWidget {
-//   @override
-//   _MyHomePageState createState() => _MyHomePageState();
-// }
-
-// class _MyHomePageState extends State<MyHomePage>
-//     with SingleTickerProviderStateMixin {
-//   double x = 0;
-//   double y = 0;
-//   late AnimationController _controller;
-//   late Animation<double> _perspectiveAnimation;
-
-//   @override
-//   void initState() {
-//     super.initState();
-//     _controller = AnimationController(
-//       duration: const Duration(seconds: 6),
-//       vsync: this,
-//     )..repeat(reverse: true);
-
-//     _perspectiveAnimation = Tween<double>(
-//       begin: 0.0,
-//       end: 1.0,
-//     ).animate(CurvedAnimation(
-//       parent: _controller,
-//       curve: Curves.easeInOut,
-//     ));
-//   }
-
-//   @override
-//   void dispose() {
-//     _controller.dispose();
-//     super.dispose();
-//   }
-
-//   @override
-//   Widget build(BuildContext context) {
-//     return Scaffold(
-//       body: Center(
-//         child: AnimatedBuilder(
-//           animation: _perspectiveAnimation,
-//           builder: (context, child) {
-//             return Transform(
-//               transform: Matrix4.identity()
-//                 ..setEntry(3, 2, _perspectiveAnimation.value)
-//                 ..rotateX(x)
-//                 ..rotateY(y),
-//               alignment: FractionalOffset.center,
-//               child: GestureDetector(
-//                 onPanUpdate: (details) {
-//                   setState(() {
-//                     y -= details.delta.dx / 100;
-//                     x += details.delta.dy / 100;
-//                   });
-//                 },
-//                 child: Container(
-//                   color: Colors.red,
-//                   height: 200.0,
-//                   width: 200.0,
-//                 ),
-//               ),
-//             );
-//           },
-//         ),
-//       ),
-//     );
-//   }
-// }
 
 class TransformApp extends StatefulWidget {
   @override
@@ -100,181 +36,229 @@ class TransformApp extends StatefulWidget {
 
 class _TransformAppState extends State<TransformApp>
     with SingleTickerProviderStateMixin {
-  double _rotateX = 0.0;
-  double _rotateY = 0.0;
-  double _rotateZ = 0.0;
-  double _translateX = 0.0;
-  double _translateY = 0.0;
-  double _translateZ = 0.0;
-  double _perspectiveValue = 0.002;
-  TextEditingController _perspectiveController = TextEditingController();
+  late AnimationController animController;
+  late Animation<double> _freeFallAnimation;
+  late Animation<double> _flipAnimation;
+  late Animation<double> _pushBackAnimation;
+  late Animation<double> _goBackUPAnimation;
+  late Animation<double> _combinedVerticalAnimation;
 
+  double _manualValue = 0;
   @override
   void initState() {
     super.initState();
-    _perspectiveController.text = _perspectiveValue.toString();
+    animController = AnimationController(
+        vsync: this, duration: const Duration(milliseconds: 6000));
+    // animController.addStatusListener((status) {
+    //   if (status == AnimationStatus.completed) {
+    //     animController.reverse();
+    //   } else if (status == AnimationStatus.dismissed) {
+    //     animController.forward();
+    //   }
+    // });
+    animController.forward();
+
+    // _freeFallAnimation = Tween<double>(begin: 0, end: 1).animate(
+    //   CurvedAnimation(
+    //     parent: animController,
+    //     curve: Interval(0.0, 0.33, curve: Curves.linear),
+    //   ),
+    // );
+
+    _flipAnimation = Tween<double>(begin: 0, end: 1).animate(
+      CurvedAnimation(
+        parent: animController,
+        curve: Interval(0.0, 0.66, curve: Curves.easeInOut
+            // const SuperDuperEaseInAndThenQuickOut(exponent: 3)
+            ),
+      ),
+    );
+
+    _pushBackAnimation = Tween<double>(begin: 0, end: 1).animate(
+      CurvedAnimation(
+        parent: animController,
+        curve: Interval(0.33, 0.66, curve: Curves.linear),
+      ),
+    );
+
+    // _goBackUPAnimation = Tween<double>(begin: 0, end: 1).animate(
+    //   CurvedAnimation(
+    //     parent: animController,
+    //     curve: Interval(0.66, 1.0, curve: Curves.linear),
+    //   ),
+    // );
+
+    // Combine vertical animations
+    _combinedVerticalAnimation = TweenSequence<double>([
+      TweenSequenceItem(
+        tween: Tween<double>(begin: 0.0, end: 200.0).chain(
+            CurveTween(curve: Interval(0.0, 0.33, curve: Curves.linear))),
+        weight: 33.0,
+      ),
+      TweenSequenceItem(
+        tween: Tween<double>(begin: 200.0, end: 200.0).chain(
+            CurveTween(curve: Interval(0.33, 0.66, curve: Curves.linear))),
+        weight: 33.0,
+      ),
+      TweenSequenceItem(
+        tween: Tween<double>(begin: 200.0, end: 0.0).chain(
+            CurveTween(curve: Interval(0.66, 1.0, curve: Curves.linear))),
+        weight: 34.0,
+      ),
+    ]).animate(animController);
   }
 
-  void _updatePerspective(String value) {
-    double? newValue = double.tryParse(value);
-    if (newValue != null) {
-      setState(() {
-        _perspectiveValue = newValue;
-        print("_updatePerspective: $_perspectiveValue");
-      });
-    }
+  @override
+  void dispose() {
+    animController.dispose();
+    super.dispose();
   }
 
   @override
   Widget build(BuildContext context) {
-    rebuildAllChildren(context);
     return Scaffold(
-      appBar: AppBar(title: Text('3D Transform Playground')),
-      body: Column(
-        children: [
-          Expanded(
-            child: Center(
-              child: Transform(
+      body: Center(
+        child: Stack(
+          alignment: Alignment.center,
+          children: [
+            Transform(
+              transform: Matrix4.identity()
+                ..setEntry(3, 2, 0.0003512553609721081)
+                ..rotateY(323 * pi / 180) // horixontal
+                ..rotateX(355 * pi / 180) // vertical
+                ..rotateZ(6 * pi / 180) //z : 32
+                ..scale(1.0),
+              alignment: Alignment.center,
+              child: _buildCardStack(),
+            ),
+            Positioned(
+                bottom: 0,
+                child: _buildSlider(
+                  value: _manualValue,
+                  onChanged: (value) => setState(() {
+                    _manualValue = value;
+                    print("_manualValue, value: $_manualValue");
+                  }),
+                  label: '_manualValue: ${_manualValue.toStringAsFixed(0)}°',
+                  // min: -200.0,
+                  // max: 200.0,
+                ))
+          ],
+        ),
+      ),
+    );
+  }
+
+  Widget _buildCardStack() {
+    return AnimatedBuilder(
+        animation: Listenable.merge([
+          // _freeFallAnimation,
+          _flipAnimation,
+          _pushBackAnimation,
+          // _goBackUPAnimation,
+          _combinedVerticalAnimation,
+        ]),
+        builder: (context, child) {
+          return Stack(
+            children: [
+              Transform(
                 transform: Matrix4.identity()
-                  ..setEntry(3, 2, _perspectiveValue)
-                  ..rotateX(_rotateX)
-                  ..rotateY(_rotateY)
-                  ..rotateZ(_rotateZ)
-                  ..translate(_translateX, _translateY, _translateZ),
-                alignment: FractionalOffset.center,
+                  ..translate(0.0, 0.0, 0.0), // -index * 50.0
                 child: Container(
                   width: 200,
                   height: 200,
-                  decoration: BoxDecoration(
-                    color: Colors.blue,
-                    border: Border.all(color: Colors.black, width: 2),
-                  ),
+                  color: Colors.green,
                   child: Center(
                     child: Text(
-                      'Perspective: ${_perspectiveValue.toStringAsFixed(6)}',
-                      textAlign: TextAlign.center,
-                      style: TextStyle(
-                        color: Colors.white,
-                        fontSize: 16,
-                        fontWeight: FontWeight.bold,
-                      ),
+                      'Layer 1',
+                      style: TextStyle(color: Colors.white),
                     ),
                   ),
                 ),
-                // Container(
-                //   width: 200,
-                //   height: 200,
-                //   decoration: const BoxDecoration(
-                //     image: DecorationImage(
-                //       image: NetworkImage('https://picsum.photos/200'),
-                //       fit: BoxFit.cover,
-                //     ),
-                //   ),
-                // ),
               ),
-            ),
-          ),
-          _buildControlPanel(),
-        ],
-      ),
-    );
-  }
-
-  void rebuildAllChildren(BuildContext context) {
-    void rebuild(Element el) {
-      el.markNeedsBuild();
-      el.visitChildren(rebuild);
-    }
-
-    (context as Element).visitChildren(rebuild);
-  }
-
-  Widget _buildControlPanel() {
-    return Container(
-      height: 300,
-      child: ListView(
-        children: [
-          _buildSlider('Rotate X', _rotateX, -pi, pi,
-              (v) => setState(() => _rotateX = v)),
-          _buildSlider('Rotate Y', _rotateY, -pi, pi,
-              (v) => setState(() => _rotateY = v)),
-          _buildSlider('Rotate Z', _rotateZ, -pi, pi,
-              (v) => setState(() => _rotateZ = v)),
-          _buildSlider('Translate X', _translateX, -100, 100,
-              (v) => setState(() => _translateX = v)),
-          _buildSlider('Translate Y', _translateY, -100, 100,
-              (v) => setState(() => _translateY = v)),
-          _buildSlider('Translate Z', _translateZ, -100, 100,
-              (v) => setState(() => _translateZ = v)),
-          _buildTextField(
-              'Perspective', _perspectiveController, _updatePerspective),
-        ],
-      ),
-    );
-  }
-
-  Widget _buildSlider(String label, double value, double min, double max,
-      ValueChanged<double> onChanged) {
-    return Padding(
-      padding: EdgeInsets.symmetric(horizontal: 16.0, vertical: 8.0),
-      child: Row(
-        children: [
-          SizedBox(
-            width: 100,
-            child: Text(label),
-          ),
-          Expanded(
-            child: Slider(
-              value: value,
-              min: min,
-              max: max,
-              onChanged: onChanged,
-            ),
-          ),
-          SizedBox(
-            width: 50,
-            child: Text(value.toStringAsFixed(2)),
-          ),
-        ],
-      ),
-    );
-  }
-
-  Widget _buildTextField(String label, TextEditingController controller,
-      ValueChanged<String> onChanged) {
-    return Padding(
-      padding: EdgeInsets.symmetric(horizontal: 16.0, vertical: 8.0),
-      child: Row(
-        children: [
-          SizedBox(width: 100, child: Text(label)),
-          Expanded(
-            child: TextField(
-              controller: controller,
-              keyboardType:
-                  TextInputType.numberWithOptions(decimal: true, signed: false),
-              textInputAction: TextInputAction.done,
-              decoration: InputDecoration(
-                border: OutlineInputBorder(),
-                suffixIcon: IconButton(
-                  icon: Icon(Icons.check),
-                  onPressed: () {
-                    onChanged(controller.text);
-                    FocusScope.of(context).unfocus();
-                  },
+              Transform(
+                transform: Matrix4.identity()
+                  ..translate(0.0, 0.0, -50.0), // -index * 50.0
+                child: Container(
+                  width: 200,
+                  height: 200,
+                  color: Colors.yellow,
+                  child: Center(
+                    child: Text(
+                      'Layer 2',
+                      style: TextStyle(color: Colors.white),
+                    ),
+                  ),
                 ),
               ),
-              onChanged: onChanged,
-              onSubmitted: (value) {
-                onChanged(value);
-                FocusScope.of(context).unfocus();
-              },
-              inputFormatters: [
-                FilteringTextInputFormatter.allow(RegExp(r'^\d*\.?\d*')),
-              ],
-            ),
-          ),
-        ],
-      ),
+              Transform(
+                transform: Matrix4.identity()
+                  ..translate(
+                      0.0,
+                      _combinedVerticalAnimation.value,
+                      // combinedLerp(
+                      //     0.0, 200.0, _freeFallAnimation, _goBackUPAnimation),
+                      // lerpDouble(0.0, 200.0, _freeFallAnimation.value)!,
+                      lerpDouble(-100.0, 0.0, _pushBackAnimation.value)!)
+                  ..rotateX(lerpDouble(
+                      0.0, 2 * pi, _flipAnimation.value)!), // -index * 50.0
+                alignment: Alignment.center,
+                child: Container(
+                  width: 200,
+                  height: 200,
+                  color: Colors.purple,
+                  child: Center(
+                    child: Text(
+                      'Layer 3',
+                      style: TextStyle(color: Colors.white),
+                    ),
+                  ),
+                ),
+              ),
+            ],
+          );
+        });
+  }
+
+  _choreo() {
+    //------------------
+    //First vinyl translates down by Y + 200
+    //First vinyl rotates
+    //------------------
+  }
+
+  double combinedLerp(
+    double start,
+    double end,
+    Animation<double> anim1,
+    Animation<double> anim2,
+  ) {
+    double value = 0.0;
+    if (!anim1.isCompleted) {
+      value = lerpDouble(start, end, anim1.value)!;
+    } else {
+      value = lerpDouble(start, end, anim2.value)!;
+    }
+    return value; // Here we're taking the maximum of the two values
+  }
+
+  Widget _buildSlider({
+    required double value,
+    required ValueChanged<double> onChanged,
+    required String label,
+    double min = 0,
+    double max = 360,
+  }) {
+    return Column(
+      children: [
+        Text(label, style: TextStyle(fontWeight: FontWeight.bold)),
+        Slider(
+          value: value,
+          min: min,
+          max: max,
+          onChanged: onChanged,
+        ),
+      ],
     );
   }
 }
