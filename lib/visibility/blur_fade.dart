2abc821ffda4e935e5bb57e7a15964078e862cff
amal chandran <precisely.amal@gmail.com>
2024-10-12 13:02:31 +0530

    Some adjustments to style

--- lib/visibility/blur_fade.dart
+++ lib/visibility/blur_fade.dart
@@ -61,9 +61,12 @@ class _BlurFadeExampleState extends State<BlurFadeExample> {
                 style: TextStyle(fontSize: 16, fontWeight: FontWeight.bold),
               ),
             ),
-            const SizedBox(height: 20),
+            const SizedBox(height: 60),
             ElevatedButton(
               onPressed: () => setState(() => _isVisible = !_isVisible),
+              style: ElevatedButton.styleFrom(
+                minimumSize: Size(150, 50),
+              ),
               child: Text(_isVisible ? 'Hide' : 'Show'),
             ),
           ],
@@ -72,99 +75,3 @@ class _BlurFadeExampleState extends State<BlurFadeExample> {
     );
   }
 }
-
-// class FadeBlurExample extends StatefulWidget {
-//   @override
-//   _FadeBlurExampleState createState() => _FadeBlurExampleState();
-// }
-
-// class _FadeBlurExampleState extends State<FadeBlurExample>
-//     with TickerProviderStateMixin {
-//   late AnimationController _controller;
-//   late Animation<double> _opacityAnimation;
-//   late Animation<double> _blurAnimation;
-
-//   @override
-//   void initState() {
-//     super.initState();
-//     _controller = AnimationController(
-//       duration: Duration(milliseconds: 500),
-//       vsync: this,
-//     );
-
-//     _opacityAnimation = Tween<double>(begin: 1.0, end: 0.0).animate(
-//       CurvedAnimation(
-//         parent: _controller,
-//         curve: Interval(0.3, 1.0, curve: Curves.linear),
-//       ),
-//     );
-
-//     _blurAnimation = Tween<double>(begin: 0.0, end: 10.0).animate(
-//       CurvedAnimation(
-//         parent: _controller,
-//         curve: const Interval(0.0, 0.7, curve: Curves.linear),
-//       ),
-//     );
-
-//     // _controller.forward();
-//   }
-
-//   @override
-//   void dispose() {
-//     _controller.dispose();
-//     super.dispose();
-//   }
-
-//   void _toggleVisibility() {
-//     if (_controller.status == AnimationStatus.completed) {
-//       _controller.reverse();
-//     } else {
-//       _controller.forward();
-//     }
-//   }
-
-//   @override
-//   Widget build(BuildContext context) {
-//     return Scaffold(
-//       body: Stack(
-//         alignment: Alignment.center,
-//         children: [
-//           Positioned(
-//             top: 0,
-//             left: 0,
-//             right: 0,
-//             bottom: 0,
-//             child: AnimatedBuilder(
-//               animation: _controller,
-//               builder: (context, child) {
-//                 return Center(
-//                   child: Opacity(
-//                     opacity: _opacityAnimation.value,
-//                     child: ImageFiltered(
-//                       imageFilter: ImageFilter.blur(
-//                         sigmaX: _blurAnimation.value,
-//                         sigmaY: _blurAnimation.value,
-//                       ),
-//                       child: Text(
-//                         'Hello, World!',
-//                         style: TextStyle(
-//                             fontSize: 24, fontWeight: FontWeight.bold),
-//                         textAlign: TextAlign.center,
-//                       ),
-//                     ),
-//                   ),
-//                 );
-//               },
-//             ),
-//           ),
-//           Positioned(
-//               bottom: 30,
-//               left: 0,
-//               right: 0,
-//               child: MaterialButton(
-//                   onPressed: _toggleVisibility, child: Text("Animate!"))),
-//         ],
-//       ),
-//     );
-//   }
-// }
